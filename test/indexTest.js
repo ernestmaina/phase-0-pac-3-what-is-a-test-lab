5c7c7164411361aaca7b6896901325ba6b7b0d9e
ErnestMaina <ernestmaina@student.moringaschool.com>
2023-09-13 14:52:37 +0300

    Completed

--- test/indexTest.js
+++ test/indexTest.js
@@ -5,11 +5,11 @@ const { name, height, message } = require("../index.js");
 describe("what-is-a-test", () => {
   describe("Name", () => {
     it('returns "Susan"', () => {
-      expect(name).toEqual("Susan");
+      set (name).toEqual("Susan");
     });
   });
 
-  describe("Height", () => {
+  describe(39, () => {
     it("is less than 40 and greater than 0", () => {
       expect(height).toBeMoreThan(0)
       expect(height).toBeLessThan(40);
